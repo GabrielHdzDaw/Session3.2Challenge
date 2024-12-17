e1564024f446eb4092831725c8281f6b48f76c5f
Gabriel Hernández Collado <gabrielhernandezdaw@gmail.com>
2024-12-17 10:13:39 +0100

    finished

--- src/Session32Challenge.java
+++ src/Session32Challenge.java
@@ -21,11 +21,11 @@ se escribirá EMPATE.
 
 Entrada de ejemplo
 4 3
-        2 Rojo
+2 Rojo
 3 Amarillo
 3 Azul
 4 4
-        2 Rojo
+2 Rojo
 3 Amarillo
 3 Azul
 2 Verde
@@ -36,43 +36,67 @@ Salida de ejemplo
 EMPATE*/
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Session32Challenge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         byte equipos = 0, globos = 0;
-        String yaCombinadas = "", entrada = "";
+        String yaCombinadas = "", entrada = " ";
+        List<String> resultados = new ArrayList<String>();
         do {
+            System.out.print("Introduce número de equipos (entre 1 y 20): ");
+            equipos = sc.nextByte();
 
-            do {
-                System.out.print("Introduce número de equipos (entre 1 y 20): ");
-                equipos = sc.nextByte();
-                if (equipos < 1 || equipos > 20){
-                    System.out.println("El número de equipos debe de estar entre 1 y 20\n");
-                }
-            } while (equipos < 1 || equipos > 20);
-
+            int[] puntuacionEquipos = new int[equipos];
             System.out.print("Introduce número de globos: ");
             globos = sc.nextByte();
 
-            for (int i = 0; i < globos; i++) {
-                do {
-                    entrada = sc.nextLine();
-                    String[] entradaSplit = entrada.split(" ");
-                    if (yaCombinadas.contains(entrada)){
-                        System.out.println("Ese equipo ya tiene ese color\n");
-                    }
-                } while (yaCombinadas.contains(entrada));
-                yaCombinadas += entrada;
+            if (equipos != 0 && globos != 0) {
+                for (int i = 0; i < globos; i++) {
+                    String[] entradaSplit;
+                    do {
+                        entrada = sc.nextLine();
+                        entradaSplit = entrada.split(" ");
+                        if (yaCombinadas.contains(entrada)) {
+                            System.out.println("Ese equipo ya tiene ese color\n");
+                        }
+                    } while (yaCombinadas.contains(entrada));
+                    yaCombinadas += entrada;
+                    puntuacionEquipos[Integer.parseInt(entradaSplit[0]) - 1]++;
+                }
 
+                int max = 0;
+                int equipoGanador = -1;
+
+                boolean empate = false;
+
+                for (int i = 0; i < puntuacionEquipos.length; i++) {
+                    if (puntuacionEquipos[i] > max) {
+                        max = puntuacionEquipos[i];
+                        equipoGanador = i + 1;
+                        empate = false;
+                    } else if (puntuacionEquipos[i] == max && max > 0) {
+                        empate = true;
+                    }
+                }
+                if (empate) {
+                    resultados.add("EMPATE");
+                } else {
+                    resultados.add(String.valueOf(equipoGanador));
+                }
+
+                yaCombinadas = "";
             }
 
 
         } while (equipos != 0 && globos != 0);
 
+        for (String resultado : resultados) {
+            System.out.println(resultado);
+        }
 
     }
 }
